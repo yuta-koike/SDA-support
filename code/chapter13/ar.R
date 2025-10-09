3bcc33d8f1a344c05bb3420ae6e153ba29ab72ff
yuta-koike <76400910+yuta-koike@users.noreply.github.com>
2025-10-09 11:23:27 +0200

    add "stats::" to the filter function

--- code/chapter13/ar.R
+++ code/chapter13/ar.R
@@ -13,7 +13,7 @@ autoplot(x)
 ggAcf(x) # 指数的に減衰
 ggPacf(x) # ラグ3以降ほぼゼロ
 ## 関数filterを使う方法(こちらの方が速い)
-y <- ts(c(x0, filter(epsilon, filter = a, method = "r", init = rev(x0))))
+y <- ts(c(x0, stats::filter(epsilon, filter = a, method = "r", init = rev(x0))))
 sum(abs(x - y)) # ほぼ0(数値計算上の問題からちょうど0でない)
 # 注．関数filterは時系列に対する線形フィルタである．
 # methodで"c"(convolution)を選ぶとmoving average, 
@@ -22,7 +22,7 @@ sum(abs(x - y)) # ほぼ0(数値計算上の問題からちょうど0でない)
 a <- c(-2,1) # a_1, a_2
 x.init <- c(0,1) # x_2, x_1
 ep <- c(5,6,7)
-(x <- filter(ep,filter = a, method = "r", init = x.init))
+(x <- stats::filter(ep,filter = a, method = "r", init = x.init))
 ## filterの出力結果の確認：
 a[1]*x.init[1]+a[2]*x.init[2]+ep[1] # x_3
 a[1]*x[1]+a[2]*x.init[1]+ep[2] # x_4
--- code/chapter13/ma.R
+++ code/chapter13/ma.R
@@ -12,7 +12,7 @@ autoplot(x)
 ggAcf(x) # 2より大きいラグでの自己相関がほぼ消滅
 ggPacf(x) # ラグ4に少しだけ偏自己相関が残っている
 ## 関数filterを使う方法(こちらの方が速い)
-y <- ts(filter(epsilon, filter = c(1, b), method = "c", sides = 1))
+y <- ts(stats::filter(epsilon, filter = c(1, b), method = "c", sides = 1))
 # sides=1 はmoving averageを過去の方向にのみ行うことを意味する．
 y[1:2] <- epsilon[1:2]
 sum(abs(x - y)) # ほぼ0(数値計算上の問題からちょうど0でない)
